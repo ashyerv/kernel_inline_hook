d1b376080f2e5e689bb0f35eb8cc2c0f9d3557d0
ashyerv <101718937+ashyerv@users.noreply.github.com>
2023-03-02 15:04:45 +0800

    Update hook.h

--- hook.h
+++ hook.h
@@ -3,7 +3,7 @@
 
 typedef INT(*LDE_DISASM)(PVOID address, INT bits);
 extern LDE_DISASM lde_disasm;
-bool lde_init();
+BOOLEAN lde_init();
 
 PVOID GetFunctionAddr(PCWSTR FunctionName);
 PVOID HookKernelApi(IN PVOID ApiAddress, IN PVOID Proxy_ApiAddress, OUT PVOID* Original_ApiAddress, OUT ULONG* PatchSize);
